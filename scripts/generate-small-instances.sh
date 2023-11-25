e0320db3502ad9ad467feb9e8d4e21f8e742149f
root <root@pa5-6-ali-dapo.novalocal>
2023-11-25 16:22:18 +0000

    added setup scripts for create instance, ssh, setup cassandra, mongodb

--- scripts/generate-small-instances.sh
+++ scripts/generate-small-instances.sh
@@ -2,11 +2,6 @@ for n in 2 3 4 5 6 7 8
 do
 	lxc launch images:ubuntu/22.04 small-instance-$n -c limits.cpu=3 -c limits.memory=12GB --vm
 	lxc config device override small-instance-$n root size=24GiB
-
-	#lxc network create br-si-$n
-	#lxc config device add small-instance-$n eno1 nic nictype=bridged parent=br-si-$n name=eno1
-
-	#lxc start small-instance-$n
 done
 
 echo "small instances generation complete. Generated 8 instances."
@@ -20,3 +15,4 @@ ufw route allow in on lxdfan0
 ufw route allow out on lxdfan0
 
 echo "Completed: network ipv4 allowed."
+echo "Please set up ssh connections"
